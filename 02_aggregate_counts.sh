8b7f3abd3b6743ff7898b1d90ab684f36797b5fb
Nicola Romano <romano.nicola@gmail.com>
2022-05-30 10:34:30 +0100

    Can choose an alternative aggr.csv filename

--- 02_aggregate_counts.sh
+++ 02_aggregate_counts.sh
@@ -4,9 +4,9 @@
 # Note: SECONDS is a bash builtin variable that gives the number of seconds since the script was started
 SECONDS=0
 
-# Check we have exactly three parameters
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <id> <output_dir> <cellranger_dir>"
+# Check we have three or four parameters
+if [ $# -ne 3 ] && [ $# -ne 4 ]; then
+    echo "Usage: $0 <id> <output_dir> <cellranger_dir> [<aggr_csv_file]"
     exit 1
 fi
 
@@ -14,12 +14,18 @@ ID=$1
 OUTPUT_DIR=$2
 CELLRANGER_DIR=$3
 
+if [ $# -eq 4 ]; then
+    AGGR_CSV=$4
+else
+    AGGR_CSV=aggr.csv
+fi
+
 # We use normalize=none as suggested here https://github.com/satijalab/seurat/issues/672
 $CELLRANGER_DIR/cellranger aggr --id=$ID \
-    --csv=$OUTPUT_DIR/aggr.csv \
+    --csv=$OUTPUT_DIR/$AGGR_CSV \
     --normalize=none
 
 # Move the output files to the output directory
 mv $ID $OUTPUT_DIR
 
-echo "Aggregation completed in $(( $SECONDS / 60 )) minutes."
+echo "Aggregation completed in $(($SECONDS / 60)) minutes."
