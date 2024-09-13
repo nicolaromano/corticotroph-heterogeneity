e5af8046f2bd329b0fd6d31f577c4f60425198b0
Nicola Romano <romano.nicola@gmail.com>
2024-09-13 16:51:33 +0100

    removed

--- 10_label_transfer_predictions.py
+++
@@ -1,7 +0,0 @@
-from label_transfer_predictions import LabelTransferPredictor
-
-predictor = LabelTransferPredictor(verbose=False)
-
-predictor.predict_all(threshold=0.6)
-
-predictor.plot_predictions_umap("Ruf_Zamojski_2021M")
